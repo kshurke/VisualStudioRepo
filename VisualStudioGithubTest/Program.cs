8c786f4289b8a28fb30e0e5ea9330a6954eee5b8
kshurke <kshurke@gmail.com>
2016-08-24 21:58:23 -0500

    Add project files.

---
+++ VisualStudioGithubTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VisualStudioGithubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing github with visual studio...");
+            Console.WriteLine("press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
